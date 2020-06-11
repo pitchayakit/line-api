9cd5d2fc4c929e0c1a8b68369c9791fc887380e8
pitchayakit <pitchayakit.p@gmail.com>
2020-06-11 22:54:46 +0800

    add line sdk add response for webhook

---
+++ app/Http/Controllers/LineController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LineController extends Controller
+{
+    public function index(){
+        http_response_code(200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/message', function () {
+    return view('welcome');
+});
+
+Route::get('/api/message', 'LineController@index');
